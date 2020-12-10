d945a238440e205ef7e684ede0dae5c61f2b49e1
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2020-12-10 14:52:50 +0100

    renderNavigation null

--- src/LaravelNovaPermission.php
+++ src/LaravelNovaPermission.php
@@ -2,79 +2,82 @@
 
 namespace Insenseanalytics\LaravelNovaPermission;
 
-use Laravel\Nova\Tool;
 use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
 
 class LaravelNovaPermission extends Tool
 {
-	public $roleResource = Role::class;
-	public $permissionResource = Permission::class;
+    public $roleResource = Role::class;
 
-	public $registerCustomResources = false;
+    public $permissionResource = Permission::class;
 
-	/**
-	 * Perform any tasks that need to happen when the tool is booted.
-	 */
-	public function boot()
-	{
-		if ((Role::class === $this->roleResource && Permission::class === $this->permissionResource)
-			|| $this->registerCustomResources) {
-			Nova::resources([
-				$this->roleResource,
-				$this->permissionResource,
-			]);
-		}
-	}
+    public $registerCustomResources = false;
 
-	/**
-	 * Set a custom Role resource class.
-	 *
-	 * @param Role resource class
-	 *
-	 * @return $this
-	 */
-	public function roleResource(string $roleResource)
-	{
-		$this->roleResource = $roleResource;
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     */
+    public function boot()
+    {
+        if ((Role::class === $this->roleResource && Permission::class === $this->permissionResource)
+            || $this->registerCustomResources) {
+            Nova::resources([
+                $this->roleResource,
+                $this->permissionResource,
+            ]);
+        }
+    }
 
-		return $this;
-	}
+    /**
+     * Set a custom Role resource class.
+     *
+     * @param Role resource class
+     *
+     * @return $this
+     */
+    public function roleResource(string $roleResource)
+    {
+        $this->roleResource = $roleResource;
 
-	/**
-	 * Set a custom Permission resource class.
-	 *
-	 * @param Permission resource class
-	 *
-	 * @return $this
-	 */
-	public function permissionResource(string $permissionResource)
-	{
-		$this->permissionResource = $permissionResource;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Set a custom Permission resource class.
+     *
+     * @param Permission resource class
+     *
+     * @return $this
+     */
+    public function permissionResource(string $permissionResource)
+    {
+        $this->permissionResource = $permissionResource;
 
-	/**
-	 * Register the custom resource classes.
-	 *
-	 * @param bool
-	 *
-	 * @return $this
-	 */
-	public function withRegistration()
-	{
-		$this->registerCustomResources = true;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Register the custom resource classes.
+     *
+     * @param bool
+     *
+     * @return $this
+     */
+    public function withRegistration()
+    {
+        $this->registerCustomResources = true;
 
-	/**
-	 * Build the view that renders the navigation links for the tool.
-	 *
-	 * @return \Illuminate\View\View
-	 */
-	public function renderNavigation()
-	{
-		return view('laravel-nova-permission::navigation');
-	}
+        return $this;
+    }
+
+    /**
+     * Build the view that renders the navigation links for the tool.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function renderNavigation()
+    {
+        return null;
+
+        return view('laravel-nova-permission::navigation');
+    }
 }
